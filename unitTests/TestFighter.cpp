c73d2d8794f9eaf413e98dd873c2e8b02225ad16
Marc Eid <marc.mn.eid@gmail.com>
2024-02-15 12:01:38 -0500

    added unit tests

---
+++ unitTests/TestFighter.cpp
@@ -0,0 +1,16 @@
+
+#include "TestFighter.h"
+
+void TestFighter::setUp(void) {
+    fighterObject = new Fighter("Fighter", 5);
+}
+
+void TestFighter::tearDown(void) {
+    delete fighterObject;
+}
+
+void TestFighter::testCalculateHitpoints() {
+    int constitutionModifier = fighterObject->modifiers["Constitution"];
+
+    CPPUNIT_ASSERT(fighterObject->hitPoints == 10 + fighterObject->getLevel() * constitutionModifier );
+}
---
+++ unitTests/TestFighter.h
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cppunit/TestCase.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/ui/text/TextTestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/XmlOutputter.h>
+
+#include "../Fighter.h"
+
+
+using namespace CppUnit;
+using namespace std;
+
+class TestFighter : public CppUnit::TestFixture
+{
+        CPPUNIT_TEST_SUITE(TestFighter);
+        CPPUNIT_TEST(testCalculateHitpoints);
+        CPPUNIT_TEST_SUITE_END();
+
+public:
+    void setUp(void);
+    void tearDown(void);
+
+protected:
+    void testCalculateHitpoints(void);
+
+
+private:
+    Fighter *fighterObject;
+
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TestFighter);//most important
