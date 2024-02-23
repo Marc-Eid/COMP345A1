95e43f5340bc292b81470ad78b9ffa68830134fc
Marc Eid <marc.mn.eid@gmail.com>
2024-02-23 14:45:02 -0500

    removed build folder from git tracking

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/marki/Documents/Github/COMP345A1
-# Build directory: C:/Users/marki/Documents/Github/COMP345A1/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
