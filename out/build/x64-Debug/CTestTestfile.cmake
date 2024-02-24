16e88fb475a6621e2b77854bacea650ee3b6b627
OliverA100 <78389672+OliverA100@users.noreply.github.com>
2024-02-23 23:50:36 -0500

    Delete out/build/x64-Debug directory

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/hp/CLionProjects/COMP345A1
-# Build directory: C:/Users/hp/CLionProjects/COMP345A1/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
