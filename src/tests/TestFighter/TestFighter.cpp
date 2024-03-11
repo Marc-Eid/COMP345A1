94ebac60196fff2b6c7f2659ebbdf2ac69240283
Marc Eid <marc.mn.eid@gmail.com>
2024-03-11 18:24:02 -0400

    added two more unit tests for the fighter class

--- src/tests/TestFighter/TestFighter.cpp
+++ src/tests/TestFighter/TestFighter.cpp
@@ -20,14 +20,33 @@ void TestFighter::tearDown(void) {
 //! test method to test the calculateHitpoints() method of the Fighter class
 //! Test Case: hitpoints == 10 + level + Con modifier
 void TestFighter::testCalculateHitpoints() {
-    int constitutionModifier = fighterObject->modifiers["Constitution"];
+    int constitutionModifier = fighterObject->getModifiers()["Constitution"];
 
-    CPPUNIT_ASSERT(fighterObject->hitPoints == 10 + fighterObject->getLevel() * constitutionModifier );
+    CPPUNIT_ASSERT(fighterObject->getHitpoints() == 10 + fighterObject->getLevel() * constitutionModifier );
 }
 
 //! test method to test the calculateAttackBonus() method of the Fighter class
 //! Test Case: attackBonus == level + Str modifier
 void TestFighter::testCalculateAttackBonus() {
 
-    CPPUNIT_ASSERT(fighterObject->getLevel() + fighterObject->modifiers["Strength"] == fighterObject->attackBonus);
+    CPPUNIT_ASSERT(fighterObject->getLevel() + fighterObject->getModifiers()["Strength"] == fighterObject->getAttackBonus());
+}
+
+void TestFighter::testAdjustLevel(void) {
+    int initialLevel = fighterObject->getLevel();
+
+    fighterObject->adjustLevel(20);
+
+    int currentLevel = fighterObject->getLevel();
+
+    CPPUNIT_ASSERT(currentLevel != initialLevel);
+}
+
+void TestFighter::testAdjustLevelAttackBonusChange(void) {
+    int initialAttackBonus = fighterObject->getAttackBonus();
+
+    fighterObject->adjustLevel(20);
+
+    int currentAttackBonus = fighterObject->getAttackBonus();
+    CPPUNIT_ASSERT(currentAttackBonus != initialAttackBonus);
 }
