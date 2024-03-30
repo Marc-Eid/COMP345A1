b11a84f3ecf5460375d51da8fb9b0bfb172f5fae
Muhammad Mubashir <mubashirqbl@gmail.com>
2024-03-29 22:56:11 -0400

    Fixed main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "classes/Fighter/Fighter.h"
 #include "classes/Map/Map.h"
 #include "classes/Dice/Dice.h"
@@ -8,36 +9,106 @@
 #include "classes/Shield/Shield.h"
 #include "classes/Ring/Ring.h"
 #include "classes/ObserverPattern/CharacterObserver.h"
+#include "classes/MapBuilders/MapEditorBuilder.h"
+#include "classes/MapBuilders/GameLevelMapBuilder.h"
+#include "classes/MapEditor/MapEditor.h"
 
 
 void displayCharacter();
 void displayStretegy();
+void displayMapBuilder();
 //void characterObserver();
 //void displayMap();
+void displayMapEditor();
 //void displayItemContainer();
 //void displayDice();
 //void displayMapBuilder();
-//void displayGameLevelMapBuilder();
+void displayGameLevelMapBuilder();
 //void displayMapEditor();
 //void displayCharacterBuilder();
+void displayFileContent(const std::string& filePath);
 
 int main() {
 
 
     displayCharacter();
-    displayStretegy();
+    displayFileContent("../src/map_example.txt");
+//    displayStretegy();
 //    displayMap();
 //    displayMapBuilder();
 //    displayGameLevelMapBuilder();
-//    displayMapEditor();
+    displayMapEditor();
 //    displayItemContainer();
 //    displayDice();
 //    displayCharacterBuilder();
 
     return 0;
+
+}
+
+void displayDice() {
+    Dice dice;
+    string diceInput;
+    cout << "Enter a string of the form xdy[+z], where:\n"
+            "\tx is the number of dice,\n"
+            "\tdy the kind of dice (d4, d6, d8, d10, d12, d20 or d100),\n"
+            "\tz is a negative of positive number added after all the dice results have been added (optional).\n"
+            "\nDice Input: ";
+    cin >> diceInput;
+    try {
+        int diceResult = dice.roll(diceInput);
+        cout << "\tRolled: " << diceInput << ", Result: " << diceResult << endl;
+        cout << "\nMore Dice Roll Examples:"<< endl;
+        cout << "\tRolled: " << "55d100-26" << ", Result: " << dice.roll("55d100-26") << endl;
+        cout << "\tRolled: " << "3d20" << ", Result: " << dice.roll("3d20") << endl;
+        cout << "\tRolled: " << "3d8-0" << ", Result: " << dice.roll("3d8-0") << endl;
+        cout << "\tRolled: " << "3d4+15" << ", Result: " << dice.roll("3d4+15") << endl;
+        cout << "\tRolled: " << "9d20+1" << ", Result: " << dice.roll("9d20+1") << endl;
+        cout << "\tRolled: " << "7d6" << ", Result: " << dice.roll("7d6") << endl;
+        cout << "\tRolled: " << "1d6-100" << ", Result: " << dice.roll("1d6-100") << endl;
+    } catch (const invalid_argument& e) {
+        cerr << "Error: " << e.what() << endl;
+    }
+
+}
+
+void displayFileContent(const std::string& filePath) {
+    std::ifstream file(filePath);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file for comparison: " << filePath << std::endl;
+        return;
+    }
+
+    std::cout << "Original map content from file:\n";
+    std::string line;
+    while (getline(file, line)) {
+        std::cout << line << std::endl;
+    }
+    std::cout << "---------------------------------" << std::endl;
+    file.close();
 }
 
 
+void displayMapBuilder() {
+    std::cout << "---------------------------------" << std::endl;
+    std::cout << "Display Map Builder: Part 4.1" << std::endl;
+    std::cout << "---------------------------------" << std::endl;
+    MapEditorBuilder builder;
+    std::string filePath = "../src/map_example.txt";
+    std::cout << "Building the Map from: " <<filePath << std::endl;
+
+    builder.loadMap(filePath);
+    Map* loadedMap = builder.getMap();
+
+    if (loadedMap != nullptr) {
+        std::cout << "Map loaded successfully.\n";
+        loadedMap->printMap(); // This will print the map to the console
+    } else {
+        std::cerr << "Failed to load map.\n";
+    }
+    displayFileContent(filePath);
+
+}
 
 void displayCharacter(){
     Fighter f1("test1", 5);
@@ -135,4 +206,50 @@ void displayStretegy(){
     // switch on/off logging sources
     npc->setLogging(false);
 
+}
+
+void displayGameLevelMapBuilder() {
+    std::cout << "---------------------------------" << std::endl;
+    std::cout << "Display Map Builder with Level Adjustments: Part 4.2" << std::endl;
+    std::cout << "---------------------------------" << std::endl;
+    GameLevelMapBuilder builder;
+    std::string filePath = "../src/map_example.txt";
+
+    builder.loadMap(filePath);
+    Map* loadedMap = builder.getMap();
+
+    std::cout << "---------------------------------" << std::endl;
+    std::cout << "Displaying the Fighter sheet before entering the game:" << std::endl;
+    std::cout << "---------------------------------" << std::endl;
+
+    Fighter *f1 = new Fighter("teemo",4);
+    loadedMap->startGame(f1,2,2);
+    f1->displayCharacterSheet();
+
+    std::cout << "---------------------------------" << std::endl;
+    std::cout << "Building the Map from: " <<filePath << std::endl;
+    std::cout << "---------------------------------" << std::endl;
+
+    if (loadedMap != nullptr) {
+        std::cout << "Map loaded successfully.\n";
+        loadedMap->printMap(); // This will print the map to the console
+    } else {
+        std::cerr << "Failed to load map.\n";
+    }
+    displayFileContent(filePath);
+
+    std::cout << "---------------------------------" << std::endl;
+    std::cout << "Displaying the Fighter sheet after entering the game with level 15:" << std::endl;
+    std::cout << "---------------------------------" << std::endl;
+
+    builder.adaptToLevel(15);
+    f1->displayCharacterSheet();
+}
+
+void displayMapEditor() {
+    std::cout << "\n\n-----------------------------------------------" << std::endl;
+    std::cout << "\tWemcome to the Map/Campaign Editor" << std::endl;
+    std::cout << "-----------------------------------------------" << std::endl;
+    MapEditor editor;
+    editor.runEditor();
 }
